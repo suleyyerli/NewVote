49fa41cf96151650d020906c2085eba5ee90b1e6
suleyyerli <yapmaa67720@gmail.Com>
2025-02-15 10:10:19 +0100

    squelize config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: 3306,
     dialect: "mysql",
     logging: process.env.NODE_ENV === "development" ? console.log : false,
     pool: {
@@ -14,6 +15,9 @@ const sequelize = new Sequelize(
       min: 0,
       acquire: 30000,
       idle: 10000,
+    },
+    dialectOptions: {
+      connectTimeout: 60000,
     },
   }
 );
