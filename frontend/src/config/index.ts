2a44753b41e167ef09f4646bb0f48f9b2d1a96de
Suleyman <yapmaa67720@gmail.com>
2025-05-02 15:04:00 +0200

    local

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,7 +1,5 @@
 const config = {
-  apiUrl:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "http://jkc88wogss8kg0ccsgwkgk08.147.93.94.82.sslip.io/api",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
 };
 
 export default config;
